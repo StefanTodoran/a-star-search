2464d1da00ba0f9334c064acfee0b3876186d0bc
StefanTodoran <stefan.alex4@gmail.com>
2024-01-31 16:27:27 -0800

    added hashset and minheap implementations from online, working on integration

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 NOTES:
 * Compiled using gcc v11.3.0
 * Program running using C18
+<!-- * Minheap implementation [link](https://github.com/armon/c-minheap-indirect) -->
+<!-- * Hashset implementation [link](https://github.com/barrust/set/tree/master) -->
--- search.c
+++ search.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include "logic.h"
+#include "heap.h"
+#include "set.h"
 
 int manhattanDistance(Position positionA, Position positionB) {
     return abs(positionA.x - positionB.x) + abs(positionA.y - positionB.y);
@@ -36,6 +38,33 @@ bool areGameStatesEqual(GameState *stateA, GameState *stateB) {
     return areBoardStatesEqual(stateA->board, stateB->board);
 }
 
+uint64_t hashGameState(const GameState *state) { // TODO: Evaluate if this function has collisions.
+    uint64_t hash = 17; // Initial value for hash.
+
+    // Combine the hash values of individual components.
+    hash = hash * 31 + ((uint64_t)state->player->x << 32 | state->player->y);
+    hash = hash * 31 + state->coins;
+    hash = hash * 31 + state->keys;
+
+    // Combine the hash value of the board.
+    for (int y = 0; y < BOARD_HEIGHT; y++) {
+        for (int x = 0; x < BOARD_WIDTH; x++) {
+            hash = hash * 31 + state->board[y][x].id;
+
+            switch (state->board[y][x].id) {
+                case BOMB:
+                    hash = hash * 31 + state->board[y][x].fuse;
+                    break;
+                case ONEWAY:
+                    hash = hash * 31 + state->board[y][x].orientation;
+                    break;
+            }
+        }
+    }
+
+    return hash;
+}
+
 typedef struct {
     GameState game;
     enum Direction direction;
@@ -60,6 +89,19 @@ typedef int (*HeuristicFunction)(GameState *state);
 
 void aStarSearch(GameState *start, HeuristicFunction heuristc) {
     //
+
+    heap frontier;
+    heap_create(&frontier, 0, NULL); // Third argument is the comparison function, defaults to treating keys as signed ints for comparison.
+
+    // Heap insertion:
+    int key = 20;
+    GameState* value = // GET SUCCESSOR HERE;
+    heap_insert(&frontier, &key, value);
+
+    // Heap removal:
+    int* key;
+    GameState* value;
+    heap_delmin(&frontier, &key, &value);
 }
 
 // SuccessorState successors[4];
