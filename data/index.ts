61b49eddbe78aa84984e7eef320c69e24b0ddb01
StefanTodoran <stefan.alex4@gmail.com>
2024-02-12 15:19:22 -0800

    first version of level fetcher should work

--- data/index.ts
+++ data/index.ts
@@ -1,26 +1,49 @@
 import fse from "fs-extra";
 import { OfficialLevelDocument, getAllEntries } from "./database";
+import { LogStatus, log } from "./util";
 
-function main() {
-    // const rawLevels: OfficialLevelDocument[] = await getAllEntries("officialLevels");
+async function getAllRawBoards() {
+    const rawLevels: OfficialLevelDocument[] = await getAllEntries("officialLevels");
+    rawLevels.sort((levelA, levelB) => levelA.order - levelB.order);
+    
+    const rawBoards: string[] = [];
+    rawLevels.forEach(rawLevel => {
+        rawBoards.push(rawLevel.board);
+    });
 
-    // for (let i = 0; i < rawLevels.length; i++) {
-    //     const rawLevel = rawLevels[i];
-    //     const existingLevel: OfficialLevel = getData(rawLevel.id);
+    return rawBoards;
+} 
 
-    //     const updatedLevel: OfficialLevel = {
-    //         uuid: rawLevel.id,
-    //         name: rawLevel.name,
-    //         board: parseCompressedBoardData(rawLevel.board),
-    //         completed: existingLevel?.completed,
-    //         official: true,
-    //         order: rawLevel.order,
-    //     };
+async function main() {
+    let writeLines = "";
+    const rawBoards = await getAllRawBoards();
+    log(`Successfully received ${rawBoards.length} levels from server.`, LogStatus.GOOD);
+    
+    rawBoards.forEach(board => {
+        writeLines += board + "\n";
+    });
+    // writeLines = writeLines.slice(writeLines.length - 1);
+    
+    const levelsFile = "levels.txt"; // TODO: Either read this from the .cpp source file or have a project level config file.
+    log(`Deleting existing levels file (${levelsFile}).`);
+    fse.removeSync(levelsFile);
 
-    //     parsedLevels.push(updatedLevel);
-    // }
+    if (fse.existsSync(levelsFile)) {
+        log(`Unable to delete ${levelsFile}, ensure file is not being edited elsewhere.`, LogStatus.ERROR);
+        return 1;
+    }
+    
+    log(`Writing new raw board data to ${levelsFile} synchronously.`);
+    fse.writeFileSync(levelsFile, writeLines);
+    
+    if (!fse.existsSync(levelsFile)) {
+        log(`Write to ${levelsFile} failed! Levels have not been saved.`, LogStatus.ERROR);
+        return 1;
+    }
 
-    // return parsedLevels;
+    log(`Successfully wrote ${rawBoards.length} raw boards to levels file!`, LogStatus.GOOD);
+    return 0;
 }
 
+log("\nFetching official levels from server...");
 main();
